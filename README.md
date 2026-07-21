b166799119e6717d9ce8faf8fc48550647010e4c
omoyelemide-svg <omoyelemide@gmail.com>
2026-07-21 08:22:00 -0700

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# busayo-portfolio
-A Personal Portfolio showcasing my UI/UX design projects, front-end development skills and case studies.
---
+++ script.js
@@ -0,0 +1,5 @@
+const contactBtn = document.getElementById("contactBtn");
+
+contactBtn.addEventListener("click", function () {
+    alert("Thanks for visiting my portfolio! Feel free to connect with me via email or LinkedIn.");
+});
